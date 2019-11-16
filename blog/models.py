870bd9206582badf9575bccfd98f7f4fa50c16a4
mochibot <amusaur83@gmail.com>
2019-11-15 19:28:05 -0800

    initial commit: installed django, ran migrations, added models and data, moved secret key to .env

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from uuid import uuid4
+
+# Create your models here.
+class Blog(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    category = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
