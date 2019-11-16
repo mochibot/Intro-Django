fead55c773caddddd00a76e64f5e0e57c44d4556
mochibot <amusaur83@gmail.com>
2019-11-15 21:46:34 -0800

    day 2 (admin interface and SQL) completed

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Blog, PersonalBlog
+
+class BlogAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'last_modified')
+
 # Register your models here.
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(PersonalBlog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from uuid import uuid4
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Blog(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -9,3 +11,6 @@ class Blog(models.Model):
     category = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalBlog(Blog):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
